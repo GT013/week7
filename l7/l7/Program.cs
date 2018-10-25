79df632715e13911a32a54aa648a4a72be5a3281
nantikan <60030049@kmitl.ac.th>
2018-10-25 22:29:24 +0700

    add

--- l7/l7/Program.cs
+++ l7/l7/Program.cs
@@ -10,36 +10,31 @@ namespace l7
     {
         static void Main(string[] args)
         {
-            Teacher teacher = new Teacher("Tom", 350f); // ประกาศคลาส Teacher teacher = สร้างวัตถุใหม่เพื่อการส่งค่าพารามิเตอร์
-            // teacher work for 20Hr/month
-            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
-                teacher.CalculateCharge(20f)); // เรียกใช้เมธอดจากคลาส Teacher มี .TypeName() และ 
-                                               // .CalculateCharge(20f)(กำหนดค่าพารามิเตอร์เท่ากับ 20) 
-            Console.ReadLine();
-        }
-    }
-    class Teacher
-    {
-        // constructor (for initial private/protected variables)
-        public Teacher(string name, float billingRate) // รับค่าพารามิเตอร์ผ่าน name และ billingRate คือ Tom,350f
-        {
-            this.name = name; // name = Tom
-            this.billingRate = billingRate; // billingRate = 350f
-        }
-        // figure out the charge based on teacher's rate
-        public float CalculateCharge(float hours) // สร้างเมธอด CalculateCharge รับค่าพารามิเตอร์ผ่าน hours
-        {
-            return (hours * billingRate); // hours = 20 , billingRate = 350 // 350*20 = 7000
-        }
-        // return the name of this type
-        public string TypeName() // สร้างเมธอด TypeName เป็นชนิดสตริง
-        {
-            return ("Teacher"); // ส่งค่าสตริงไปแสดงเมื่อเมธอดถูกเรียกใช้
-        }
-        private string name; // ข้อมูล name จะไม่ถูกเข้าถึง
-        protected float billingRate; // เมธอดลูกสามารถเข้าถึงได้
-    }
+            Program p = new Program();
+            p.print(5);
+            p.print((5.26f).ToString());
+            p.print(264.2871);
+            p.print((int)1354.23);
+            p.print("Hello World of C#");
+            Console.ReadKey();
 
+        }
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
+    }
 
 }
 
